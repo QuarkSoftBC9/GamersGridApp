15c45cb66a0182bdb70f82be2e3490871a3e482f
JohnLez <jeanlezineau@gmail.com>
2020-03-19 16:59:40 +0200

    Partial view rendering for correct chatroom on message board

--- GamersGridApp/Controllers/MessageController.cs
+++ GamersGridApp/Controllers/MessageController.cs
@@ -10,32 +10,54 @@ using System.Data.Entity;
 
 namespace GamersGridApp.Controllers
 {
+    [Authorize]
     public class MessageController : Controller
     {
         // GET: Message
         private ApplicationDbContext db = new ApplicationDbContext();
         //var currentUser = db.Users.Where(u => u.Id == CurrentUserID).SingleOrDefault();
 
-        [Authorize]
         public ActionResult MessageBoard()
         {
             var CurrentUserID = User.Identity.GetUserId();
             var currentUser = db.Users.Where(u =>u.Id==CurrentUserID).Select(u=>u.UserAccount).SingleOrDefault();
-
-            var viewmodel = new MessageBoardViewModel
-            {
-                MessageChats = db.MessageChats
+            var messageChats = db.MessageChats
                 .Include(m => m.Users)
                 .Include(m => m.ChatHistory)
                 .Where(m => m.Users.Contains(db.Users.Where(u => u.Id == CurrentUserID).Select(u => u.UserAccount).FirstOrDefault()))
-                .ToList(),
-                CurrentUserNickName = currentUser.NickName,
-                CurrentChatID = 1
+                .ToList();
 
+            var currentChat = messageChats.Take(1).SingleOrDefault();
+
+            var viewmodel = new MessageBoardViewModel
+            {
+                MessageChats = messageChats,
+                CurrentUserNickName = currentUser.NickName,
+                CurrentChatID = currentChat.ID
             };
 
 
             return View(viewmodel);
+        }
+
+        public ActionResult GetPartial(string chatId)
+        {
+            int id = int.Parse(chatId);
+            var aspNetUserId = User.Identity.GetUserId();
+            var user = db.Users.Where(u => u.Id == aspNetUserId).Select(u => u.UserAccount).SingleOrDefault();
+
+            var viewModel = new MessageBoardViewModel()
+            {
+                CurrentChatID = id,
+                CurrentUserNickName = user.NickName,
+                MessageChats = db.MessageChats
+                       .Include( c => c.ChatHistory)
+                       .Include(m => m.Users)
+                       .Where(m => m.ID == id)
+                       .ToList()
+            };
+
+            return PartialView("_ChatBox", viewModel);
         }
     }
 }
