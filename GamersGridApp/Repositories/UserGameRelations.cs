816bda92315314d406741453e63f7ec4a2ae8dfb
Αβραάμ Λιαούτσης <av.liaoutsis@outlook.com>
2020-04-16 12:48:12 +0300

    add some repos

--- GamersGridApp/Repositories/UserGameRelations.cs
+++ GamersGridApp/Repositories/UserGameRelations.cs
@@ -1,6 +1,7 @@
 ﻿using GamersGridApp.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -13,6 +14,22 @@ namespace GamersGridApp.Repositories
         public UserGameRelations(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        public List<UserGame> GetUserGamesStats(int userid)
+        {
+            return _context.UserGameRelations
+                .Where(u => u.UserId == userid)
+                .Include(g => g.Game)
+                .Include(ga => ga.GameAccount)
+                .Include(gs => gs.GameAccount.GameAccountStats).ToList();
+        }
+        public Dictionary<string,GameAccountStats> GetGamesTitlesDict(List<UserGame> usergamelist,int userid)
+        {
+            return usergamelist
+                .Where(u => u.UserId == userid)
+                .Select(ga => ga.GameAccount.GameAccountStats)
+                .ToDictionary(g => g.GameAccount.UserGame.Game.Title);
         }
 
 
---
+++ GamersGridApp/Repositories/UserNotificationRepository.cs
@@ -0,0 +1,28 @@
+﻿using GamersGridApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GamersGridApp.Repositories
+{
+    public class UserNotificationRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserNotificationRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Notification> GetUserNotifications(int  userid)
+        {
+            return _context.UserNotifications
+                          .Where(un => un.UserId == userid && !un.IsRead)
+                          .Include(un => un.Notification)
+                          .Select(n => n.Notification)
+                          .ToList();
+        }
+    }
+}
