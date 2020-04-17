56c1256265b82f6c8d28f59a4413e6f2641da317
Maria Ντ.ακη <marpedaki@gmail.com>
2020-04-18 01:17:03 +0300

    LeaderBoard

---
+++ GamersGridApp/ViewModels/LeaderBoardViewModel.cs
@@ -0,0 +1,24 @@
+﻿using GamersGridApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GamersGridApp.ViewModels
+{
+    public class LeaderBoardViewModel
+    {
+        public List<GameAccountStats> TopLolStats { get; set; }
+        public List<GameAccountStats> TopDotaStats { get; set; }
+        public List<GameAccountStats> TopOverwatchStats { get; set; }
+
+
+        public LeaderBoardViewModel()
+        {
+            TopLolStats = new List<GameAccountStats>();
+            TopDotaStats = new List<GameAccountStats>();
+            TopOverwatchStats = new List<GameAccountStats>();
+        }
+    }
+
+}
