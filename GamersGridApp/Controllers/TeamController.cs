95e65a4dbc7dfb6986f5c9ae498ef01fbfeb11f4
Sonik08 <stanislavnovikov1@gmail.com>
2020-04-22 19:08:33 +0300

    Dota search

---
+++ GamersGridApp/Controllers/TeamController.cs
@@ -0,0 +1,59 @@
+﻿using GamersGridApp.Core;
+using GamersGridApp.Core.Models;
+using GamersGridApp.Core.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GamersGridApp.Controllers
+{
+    [Authorize]
+    public class TeamController : Controller
+    {
+        private readonly IUnitOfWork UnitOfWork;
+
+        public TeamController(IUnitOfWork unitofwork)
+        {
+            UnitOfWork = unitofwork;
+        }
+
+        // GET: Teams
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Team(int id)
+        {
+            var appUserId = User.Identity.GetUserId();
+            var user = UnitOfWork.GGUsers.GetUser(appUserId);
+            var team = UnitOfWork.Teams.GetTeam(id);
+
+            return View(team);
+        }
+        public ActionResult New()
+        {
+            var appUserId = User.Identity.GetUserId();
+            var user = UnitOfWork.GGUsers.GetUserContent(appUserId);
+            NewTeamViewModel viewModel = new NewTeamViewModel(new Team(user.ID), UnitOfWork.Games.GetGames());
+            return View(viewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult New(NewTeamViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Games = UnitOfWork.Games.GetGames();
+                return View("New", viewModel);
+            }
+            var team = viewModel.Team;
+            UnitOfWork.Teams.AddTeam(team);
+            UnitOfWork.Complete();
+            var teamSaved = UnitOfWork.Teams.GetTeamByName(team.Name);
+            return RedirectToAction("Team", "Team", teamSaved.ID );
+        }
+    }
+}
---
+++ GamersGridApp/Persistence/Repositories/TeamRepository.cs
@@ -0,0 +1,36 @@
+﻿using GamersGridApp.Core.Models;
+using GamersGridApp.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GamersGridApp.Persistence.Repositories
+{
+    public class TeamRepository : ITeamRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public TeamRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public Team GetTeam(int id)
+        {
+            return _context.Teams.Where(t => t.ID == id)
+                .Include(tu => tu.TeamUsers.Select(u => u.User))
+                .Include(a => a.Admin)
+                .Include(g => g.Game)
+                .SingleOrDefault();
+        }
+        public void AddTeam(Team team)
+        {
+            _context.Teams.Add(team);
+        }
+        public Team GetTeamByName(string name)
+        {
+            return _context.Teams
+                .SingleOrDefault(t => t.Name == name);
+        }
+    }
+}
